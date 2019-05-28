da100c055cdd93c4631484977853b8ec3a4f13b4
viteo-lab <viteo@rambler.ru>
2019-05-28 14:58:52 +0300

    external control and sensor library

---
+++ TinyCO2_External/MHZ19uart/MHZ19.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+
+namespace MHZ19uart
+{
+    public class MHZ19 : INotifyPropertyChanged
+    {
+        private SerialPort _serialPort;
+        private int gasConcn;
+        private int temperature;
+        private static readonly byte[] dataRequest = { 0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
+        //public static readonly byte[] calibZero = { 0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78 };
+        //public static readonly byte[] calibSpan = { 0xff, 0x01, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x77 };
+        //public static readonly byte[] calibAuto_on = { 0xff, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00, 0xe6 };
+        //public static readonly byte[] calibAuto_off = { 0xff, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86 };
+
+        /// <summary>Gas Concentration</summary>
+        /// <value>PPM - particles per million</value>
+        public int GasConcn { get => gasConcn; private set { gasConcn = value; OnPropertyChanged(); } }
+        /// <summary>Sensor temperature</summary>
+        /// <value>Celsius</value>
+        public int Temperature { get => temperature; private set { temperature = value; OnPropertyChanged(); } }
+
+        public MHZ19(string portName)
+        {
+            _serialPort = new SerialPort(portName);
+            _serialPort.BaudRate = 9600;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Open();
+            _serialPort.DataReceived += DataReceivedHandler;
+        }
+
+        public void RequestData()
+        {
+            Write(dataRequest);
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            byte[] response = new byte[sp.BytesToRead];
+            sp.Read(response, 0, response.Length);
+            if (response.Length == 9 && response[8] == GetCheckSum(response))
+            {
+                Parse(response);
+            }
+        }
+
+        private void Parse(byte[] packet)
+        {
+            switch (packet[1])
+            {
+                case 0x86: // gas concentration and temperature
+                    GasConcn = btoi(packet[2], packet[3]);
+                    Temperature = packet[4] - 40;
+                    OnPropertyChanged("Data");
+                    break;
+                default: return;
+            }
+        }
+
+        private int btoi(byte high, byte low)
+        {
+            return (256 * high) + low;
+        }
+
+        private byte GetCheckSum(byte[] packet)
+        {
+            byte checkSum = 0;
+            for (int i = 1; i < 8; i++)
+                checkSum += packet[i];
+            checkSum = (byte)(0xff - checkSum);
+            checkSum++;
+            return checkSum;
+        }
+
+        private void Write(byte[] command)
+        {
+            _serialPort.Write(command, 0, command.Length);
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
---
+++ TinyCO2_External/TestConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using MHZ19uart;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        private static void PropertyChangedHandler(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Data")
+                Console.WriteLine(((MHZ19)sender).GasConcn + " | " + 
+                    ((MHZ19)sender).Temperature + " | " +
+                    DateTime.Now.ToLongTimeString());
+        }
+        static void Main(string[] args)
+        {
+            var sensor = new MHZ19("COM5");
+            sensor.PropertyChanged += PropertyChangedHandler;
+
+            var timer = new Timer(e => { sensor.RequestData(); }, null, TimeSpan.Zero, TimeSpan.FromSeconds(8));
+
+            Console.WriteLine("Press any key to close stop . . .");
+            Console.ReadLine();
+        }
+    }
+}
